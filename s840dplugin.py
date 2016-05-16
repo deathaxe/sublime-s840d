1bf34310d1a9f0dab2fd452920b3cc2099d77dbd
DeathAxe <deathaxe@web.de>
2016-05-16 14:17:33 +0200

    Restructured s840dplugin for better performance.
    SublimeText's API is very slow, which causes the operations to take several seconds on larger files.
    Therefore the whole content is now read from sublime's buffer and operations are performed
    on the text directly. Finally the whole text is pushed back to the view buffer.
    
    Renumbering is now done without regexp to maximize performance. Altogether this speeds up
    renumbering by a factor of 1000.
    
    The only remaining bottleneck is the update of sublime's text buffer, which takes several seconds
    for larger files with around 10.000 or more lines.

--- s840dplugin.py
+++ s840dplugin.py
@@ -1,19 +1,7 @@
 import sublime, sublime_plugin
+import math
 import re
-import sre_compile
 import cProfile, pstats
-
-_regexp_cache = dict()
-
-def regexp_match(pattern, text):
-  if not pattern in _regexp_cache:
-    _regexp_cache[pattern] = sre_compile.compile(pattern)
-  return _regexp_cache[pattern].match(text)
-
-def regexp_replace(pattern, replace, text):
-  if not pattern in _regexp_cache:
-    _regexp_cache[pattern] = sre_compile.compile(pattern)
-  return _regexp_cache[pattern].sub(replace, text)
 
 
 class S840dTextCommand(sublime_plugin.TextCommand):
@@ -51,29 +39,32 @@ class S840dMinifyCommand(S840dTextCommand):
 
   # API method
   def run(self, edit):
+
+    # pr = cProfile.Profile()
+    # pr.enable()
+
     # run only for SINUMERIK code
     if (self.is_scope_s840d()):
-      line = self.view.line(0)
-      while (line.b <= self.view.size()):
-        # line content without leading and tailing whitespaces
-        text = self.view.substr(line).strip()
-        # remove block numbers
-        text = regexp_replace(r'^[Nn]\d+\s*', '', text)
-        # remove comments
-        text = regexp_replace(r'\s*;(?!\$PATH\=).*$', '', text)
-        # no whitespaces around operators or seperators
-        text = regexp_replace(r'\s*([-+*/=><,:\[\(\]\)]+)\s*', r'\1', text)
-        # remove multi space
-        text = regexp_replace(r'\s{2,}', ' ', text)
-        if (len(text)>0):
-          # replace line content by minified version
-          self.view.replace(edit, line, text)
-        else:
-          # remove the whole line including the line feed
-          line.b += 1
-          self.view.erase(edit, line)
-        # move on to the next line
-        line = self.view.line(line.a + len(text) + 1)
+      view = self.view
+
+      # get a copy of the file content
+      region = sublime.Region(0, view.size())
+      text = view.substr(region)
+      # remove leading line spaces and block numbers
+      text = re.sub(r'(?im)^(?:\s*N\d+)?\s*', '', text)
+      # remove comments excluding some special header comments
+      text = re.sub(r'(?im)\s*;(?!(?:\$PATH\=|DATE|VERSION|CHANGE)).*$', '', text)
+      # no whitespaces around operators or seperators
+      text = re.sub(r'[\t ]*([-+*/=><,\:\[\(\]\)]+)[\t ]*', r'\1', text)
+      # remove multi space
+      text = re.sub(r'[\t ]{2,}', ' ', text)
+      # replace view's content
+      view.replace(edit, region, text)
+
+    # pr.disable()
+    # sortby = 'cumulative'
+    # ps = pstats.Stats(pr).sort_stats(sortby)
+    # ps.print_stats()
 
 
 class S840dBeautifyCommand(S840dTextCommand):
@@ -85,37 +76,50 @@ class S840dBeautifyCommand(S840dTextCommand):
   """
 
   indents = 0
-  tab_size = 1
+  tab_size = 2
 
   # API method
   def run(self, edit):
 
-    pr = cProfile.Profile()
-    pr.enable()
+    # pr = cProfile.Profile()
+    # pr.enable()
 
     # run only for SINUMERIK code
     if (self.is_scope_s840d()):
-      line = self.view.line(0)
-      self.tab_size = self.view.settings().get('tab_size')
-      while (line.b <= self.view.size()):
-        # line content without leading and tailing whitespaces
-        text = self.view.substr(line)
+      view = self.view
+      # get a copy of the file content
+      view_region = sublime.Region(0, view.size())
 
-        #text = self.beautify(text)
-        # replace line content by correctly indented one
-        self.view.replace(edit, line, text)
-        # move on to the next row
-        line = self.view.line(line.a + len(text) + 1)
+      # this is basically what sublime's reindent does
+      repl = ''
+      for line in re.sub(r'(?im)^[ \t]*N\d+[ \t]*', '', view.substr(view_region)).split('\n'):
+        repl += self.__indentdify(line.strip()) + '\n'
 
-    pr.disable()
-    sortby = 'cumulative'
-    ps = pstats.Stats(pr).sort_stats(sortby)
-    ps.print_stats()
+      # surround IF condition with parentheses
+      # this is not required but looks better
+      repl = re.sub(r'(?i)^[ \t]*IF\b[ \t]*([\w\d\$].*?(?=GOTO|;|$))', r'IF (\1) ', repl)
+      # one whitespace before and after literal operators
+      repl = re.sub(r'(?i)[ \t]*\bNOT\b[ \t]*', r' NOT ', repl)
+      # no whitespaces around ( or [
+      repl = re.sub(r'[ \t]*([\[\(]+)[ \t]*', r'\1', repl)
+      # one whitespace before and after literal operators
+      repl = re.sub(r'(?i)[ \t]*\b(AND|OR|XOR|B_AND|B_OR|B_XOR|B_NOT|MOD|DIV)\b[ \t]*', r' \1 ', repl)
+      # one whitespace after ) or ]
+      repl = re.sub(r'[ \t]*([\]\)\:]+)[ \t]*', r'\1 ', repl)
+      # no whitespace around unary operators
+      repl = re.sub(r'[ \t]*([-+*/=><,]+)[ \t]*', r'\1', repl)
+      # one whitespace after keyword
+      repl = re.sub(r'(?i)\b(IF|FOR|LOOP|UNTIL|WHILE)\b[ \t]*', r'\1 ', repl)
 
-  # private member
-  def beautify(self,text):
-    # remove block numbers and leading spaces
-    text = regexp_replace(r'^(?:\s*[Nn]\d+)?\s*', r'', text)
+      # replace view's content and keep the last empty line only
+      view.replace(edit, view_region, repl.strip() + '\n')
+
+    # pr.disable()
+    # sortby = 'name' # 'cumulative'
+    # ps = pstats.Stats(pr)#.sort_stats(sortby)
+    # ps.print_stats()
+
+  def __indentdify(self,text):
     # block start
     if regexp_match(r'^(?i)(?:IF|FOR|LOOP|REPEAT|WHILE)\b', text):
       text = ' ' * self.indents + text
@@ -134,24 +138,6 @@ class S840dBeautifyCommand(S840dTextCommand):
         if not regexp_match(r'^\w+:', text):
           text = ' ' * self.indents + text
 
-    # ignore comments
-    if not regexp_match('^\s*;', text):
-      # surround IF condition with parentheses
-      # this is not required but looks better
-      text = regexp_replace(r'(?i)\bIF\b\s*([\w\d\$].*?(?=GOTO|;|$))', r'IF (\1) ', text)
-      # one whitespace before and after literal operators
-      text = regexp_replace(r'(?i)\s*\bNOT\b\s*', r' NOT ', text)
-      # no whitespaces around ( or [
-      text = regexp_replace(r'\s*([\[\(]+)\s*', r'\1', text)
-      # one whitespace before and after literal operators
-      text = regexp_replace(r'(?i)\s*\b(AND|OR|XOR|B_AND|B_OR|B_XOR|B_NOT|MOD|DIV)\b\s*', r' \1 ', text)
-      # one whitespace after ) or ]
-      text = regexp_replace(r'\s*([\]\):]+)\s*', r'\1 ', text)
-      # no whitespace around unary operators
-      text = regexp_replace(r'\s*([-+*/=><,]+)\s*', r'\1', text)
-      # one whitespace after keyword
-      text = regexp_replace(r'(?i)\b(IF|FOR|LOOP|UNTIL|WHILE)\s*', r'\1 ', text)
-
     return text
 
 
@@ -159,47 +145,65 @@ class S840dRenumberCommand(S840dTextCommand):
   """
   Add or update block numbers.
 
-  Each CNC block normally starts with a block number for identification.
+  Each CNC block normally starts with a number for identification.
   After editing the numbers may be mixed up. This command helps to
   update the block numbers.
   """
 
-  blockno = 1
-  blockno_step = 10
-
   # API method
   def run(self, edit):
-    view = self.view
+
     # run only for SINUMERIK code
     if (self.is_scope_s840d()):
-      # calculate first blocknumber
-      self.auto_blockstart()
-      line = view.line(0)
-      while (line.end() <= view.size()):
-        # line content without leading and tailing whitespaces
-        text = self.renumber(view.substr(line))
-        # replace line content by correctly indented one
-        view.replace(edit, line, text)
-        # move on to the next row
-        line = view.line(line.begin() + len(text) + 1)
+      view = self.view
+      # build whole content's region
+      view_region = sublime.Region(0, view.size())
+      # create a list of rows
+      rows = view.substr(view_region).split('\n')
+      # determine first block number so that all blocknumbers
+      # will have the same amount of digits
+      blockno_step = 10
+      num_digits = int(math.log10(len(rows) * blockno_step))
+      blockno = 10 ** num_digits
+      # Add block numbers to each row which is not
+      # empty or a comment. Try to keep indented
+      # comments in position with blocks
+      repl = ''
+      for row in rows:
+        # not an empty row
+        if row:
+          # unindented line comment
+          if row[0] == ';':
+            repl += row
 
-  def auto_blockstart(self):
-    """
-    Calculate first block number from the number of rows in the file
-    and the given step witdth so that the number of digits of each
-    block number is always the same.
-    """
-    rows = self.view.rowcol(self.view.size() - 1)[0]
-    self.blockno = 1
-    while (rows * self.blockno_step >= 10):
-      self.blockno *= 10
-      rows /= 10
+          else:
+            i = 0
+            # skip leading white space
+            while row[i] == ' ':
+              i += 1
 
-  # private member
-  def renumber(self,text):
-    # comment
-    if not regexp_match(r'^\s*(?:[%;]|$)', text):
-      text = regexp_replace(r"^(\s*[Nn]\d+\s?)?", "N" + str(self.blockno) + " " , text)
-      self.blockno += self.blockno_step
+            # indented line comment
+            if row[i] == ';':
+              # insert spaces instead of 'Nxxx '
+              repl += ' ' * (2 + num_digits) + row
 
-    return text
+            else:
+              # skip existing block number
+              if row[i] == 'N':
+                i += 1
+                while row[i] >= '0' and row[i] <= '9':
+                  i += 1
+                # skip one whitespace after block number
+                # as it will be added anyway in the next step
+                if row[i] == ' ':
+                  i += 1
+
+              # ordinary block
+              repl += 'N' + str(blockno) + ' ' + row[i:]
+              blockno += blockno_step
+
+        # finalize the row
+        repl += '\n'
+
+      # replace view's content and keep the last empty line only
+      view.replace(edit, view_region, repl.strip() + '\n')
