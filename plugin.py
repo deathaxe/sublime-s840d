7216a70a60262834c8a61224a18ec2dfea1a8d3d
DeathAxe <deathaxe@web.de>
2017-02-11 17:25:38 +0100

    Other: Update syntax specific files with <template>.sublime-settings
    Instead of hard coding essential settings, they are again stored in usual
    sublime-settings files. As ST's load_settings() sometimes ignores settings
    files whose name matches a source scope, they need to be loaded and copied
    to the syntax settings file manually. This also ensures essential settings
    to be applied for the three syntaxes.
    
    The code is moved to the plugin.py.

--- plugin.py
+++ plugin.py
@@ -7,3 +7,39 @@ import sublime
 from .gcode.beautify import S840dBeautifyCommand
 from .gcode.minify import S840dMinifyCommand
 from .gcode.blockno import S840dRenumberCommand
+
+
+def plugin_loaded():
+    """This function is called by ST3 after all plugins are loaded."""
+    apply_essential_settings("s840d_arc")
+    apply_essential_settings("s840d_gcode")
+    apply_essential_settings("s840d_hmi")
+
+
+def apply_essential_settings(syntax):
+    """Apply essential settings from template to user file.
+
+    This function loads the default values from a syntax settings file
+    coming with this package as resource and copies all values to the
+    /Packages/User/<syntax>.sublime-settings.
+    """
+    dirty = False
+    file_name = "%s.sublime-settings" % syntax
+    settings = sublime.load_settings(file_name)
+    template = sublime.decode_value(sublime.load_resource(
+        sublime.find_resources(file_name)[0]))
+    for key in template.keys():
+        value = template[key]
+        # An essential setting was changed and needs update in the user syntax
+        # settings file.
+        if value != settings.get(key):
+            dirty = True
+            if value:
+                # Apply essential settings.
+                settings.set(key, value)
+            else:
+                # Keys to remove are marked with 'None' value.
+                settings.erase(key)
+    if dirty:
+        # Save settings if at least one setting was changed.
+        sublime.save_settings(file_name)
