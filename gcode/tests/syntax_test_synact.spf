deathaxe/sublime-s840d

commit 2debd42f086da054c1c49b4f9d64873112a1b0a6

G-Code: Add cycle/procedure call highlighting to synchronous actions

gcode/s840d_gcode.sublime-syntax
@@ -1028,9 +1028,16 @@ contexts:
   stmt-synact-operation:
     - match: (?i:DO)\b
       scope: keyword.control.synact.operation.s840d_gcode
-      set:
-        - meta_content_scope: meta.synact.s840d_gcode
-        - include: stmt-iso-body
+      set: [stmt-synact-operation-iso, stmt-synact-operation-cycle]
+
+  stmt-synact-operation-iso:
+    - meta_content_scope: meta.synact.operation.s840d_gcode
+    - include: stmt-iso-body
+
+  stmt-synact-operation-cycle:
+    - include: stmt-procedure-call
+    - include: stmt-cycle-call
+    - include: scope-end
 
 ###############################################################################
 # WHILE ... ENDWHILE

gcode/tests/syntax_test_synact.spf
@@ -60,6 +60,24 @@ N5 WHENEVER ABS(R1)==10 DO M5=SQRT(-R2)*$PI
 ;                             ^^^^ support.function
 ;                                 ^ punctuation.section.arguments.begin
 ;                                     ^ punctuation.section.arguments.end
+; modal synchronous function calling a built-in procedure
+N7 WHENEVER $A_IN[1] DO RDISABLE
+;                      ^^^^^^^^^ meta.synact.operation
+;  ^^^^^^^^ keyword.control.synact.condition
+;           ^^^^^ support.variable.nck
+;                    ^^ keyword.control.synact.operation
+;                       ^^^^^^^^ support.function.nck.procedure
+; modal synchronous function to calculate something
+N7 WHENEVER $A_IN[1] DO VARIABLE = 1 + VAR2
+;                      ^^^^^^^^^^^^^^^^^^^^ meta.synact.operation
+;  ^^^^^^^^ keyword.control.synact.condition
+;           ^^^^^ support.variable.nck
+;                    ^^ keyword.control.synact.operation
+;                       ^^^^^^^^ variable.other
+;                                ^ keyword.operator.assignment
+;                                  ^ constant.numeric
+;                                    ^ keyword.operator.arithmetic
+;                                      ^^^^ variable.other
 
 ; modal synact.ronous positioning call
 N6 ID=1 EVERY $A_DBB[5]<>0 DO POS[Q1]=$A_DBB[5]

commit 099820d84e541ac0f4d6b84c7608ccdb51c27368

G-Code: Fix wrong scope after synchronous action
Introduced by commit 2debd42

This commit ensures to pop off the scope meta.synact.operation on end of line.

gcode/s840d_gcode.sublime-syntax
@@ -1029,6 +1029,10 @@ contexts:
     - match: (?i:DO)\b
       scope: keyword.control.synact.operation.s840d_gcode
       set: [stmt-synact-operation-iso, stmt-synact-operation-cycle]
+      with_prototype:
+        # make sure to pop off before line end from all statements
+        - match: $
+          pop: true
 
   stmt-synact-operation-iso:
     - meta_content_scope: meta.synact.operation.s840d_gcode

gcode/tests/syntax_test_synact.spf
@@ -78,6 +78,13 @@ N7 WHENEVER $A_IN[1] DO VARIABLE = 1 + VAR2
 ;                                  ^ constant.numeric
 ;                                    ^ keyword.operator.arithmetic
 ;                                      ^^^^ variable.other
+; ensure line after synchronous action is highlighted correctly
+N7 WHENEVER $A_IN[1] DO RDISABLE
+STOPRE
+;<- keyword.control.stopre
+N7 WHENEVER $A_IN[1] DO VARIABLE = 1 + VAR2
+STOPRE
+;<- keyword.control.stopre
 
 ; modal synchronous positioning call
 N6 ID=1 EVERY $A_DBB[5]<>0 DO POS[Q1]=$A_DBB[5]