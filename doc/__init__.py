ad7629373b297be502659afe416979f12e436706
DeathAxe <deathaxe@web.de>
2017-11-04 21:58:57 +0100

    G-Code: Use multiprocessing to initialize doc database
    The plugin_host is quite busy at startup, so let another process
    initialize the tooltip cache, which improves speed a lot.

--- doc/__init__.py
+++ doc/__init__.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
-import sublime
+import multiprocessing
 
 from . import database
 
 
 def plugin_loaded():
     """Called by SublimeText after all plugins have been loaded."""
-    sublime.set_timeout_async(lambda: database.cache_init(False), 5000)
+    multiprocessing.Process(target=lambda: database.cache_init(False)).run()
