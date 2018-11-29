e2ac26cdc319c4ebdbdc4a0804c8f2d9e6c01a1d
DeathAxe <deathaxe@googlemail.com>
2018-11-29 20:51:40 +0100

    G-Code: Fix minify command
    The minify command could have caused the code to break if a string contains the `;` character. Furthermore it removed all multiple spaces in strings which may break log file creation or simple string formating functions.

--- gcode/commands/minify.py
+++ gcode/commands/minify.py
@@ -7,13 +7,40 @@ from . import base
 def minify_code(text):
     # remove leading line spaces and block numbers
     text = re.sub(r'(?im)^(?:\s*N\d+)?\s*', '', text)
-    # remove comments excluding some special header comments
-    text = re.sub(r'(?im)\s*;\s*(?!\$PATH\=|DATE|VERSION|CHANGE).*$', '', text)
-    # no whitespaces around operators or seperators
-    text = re.sub(r'[\t ]*([-+*/=><,\:\[\(\]\)]+)[\t ]*', r'\1', text)
-    # remove multi space
-    text = re.sub(r'[\t ]{2,}', ' ', text)
+    # remove whitespaces around operators or seperators
+    text = re.sub(r'[\t ]*([-+*/=><,\[\(\]\)]+)[\t ]*', r'\1', text)
+    # remove whitespaces after double colon if not preceded by quots
+    text = re.sub(r'(^[^""]*?\:+?)[\t ]*', r'\1', text)
+    # remove empty or blank lines
+    text = re.sub(r'(\s*\n){2,}', '\n', text)
     return text
+
+
+def replace_comment(view, edit, region, excludes):
+    for v in excludes:
+        if region.contains(v):
+            view.replace(edit, region, view.substr(v))
+            excludes.remove(v)
+            return True
+    return False
+
+
+def remove_comments(view, edit):
+    """Remove all but some special comments.
+
+    Remove comments by scope as ; can be part of a string which makes it
+    hard and error prone to handle via regexp only.
+    """
+    versions = view.find_all("^;VERSION:.*?\n")
+    paths = view.find_all(r";\$PATH=.*?\n")
+    for region in reversed(view.find_by_selector("comment.line")):
+        if replace_comment(view, edit, region, versions):
+            continue
+        if replace_comment(view, edit, region, paths):
+            continue
+        # don't remove the newline character which comments end with
+        region.b -= 1
+        view.erase(edit, region)
 
 
 class S840dMinifyCommand(base.TextCommand):
@@ -31,6 +58,9 @@ class S840dMinifyCommand(base.TextCommand):
         """
         # backup cursor and viewport position
         row, vp_y = self.backup_viewport()
+
+        remove_comments(self.view, edit)
+
         # get a copy of the file content
         region = sublime.Region(0, self.view.size())
         text = self.view.substr(region)
