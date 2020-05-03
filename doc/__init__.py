0e339911e23ad1e04bc43a6b7ffd99d4e59139ab
DeathAxe <deathaxe82@googlemail.com>
2020-05-03 18:57:53 +0200

    Docs: Fix documentation module initialization
    Recent builds of ST do no longer run `plugin_loaded()` from submodules.
    
    Thus its added to the main module, also turning multiprocessing into
    threading. The former one worked by accident as it was used in a wrong
    way.

--- doc/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-import multiprocessing
-
-from . import database
-
-
-def plugin_loaded():
-    """Called by SublimeText after all plugins have been loaded."""
-    multiprocessing.Process(target=lambda: database.cache_init(False)).run()
